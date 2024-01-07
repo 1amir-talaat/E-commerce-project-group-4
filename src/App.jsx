2562174593415c8480e5c67c1108b37c081b60d3
amir talaat <hsiwh2be872b@gmail.com>
2024-01-07 19:38:50 +0200

    init commet 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   );
