b37d31443a5e1d8d1b18ab98983e169429450ba1
amir talaat <hsiwh2be872b@gmail.com>
2024-01-19 00:01:40 +0200

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,33 +2,13 @@ import React, { useEffect, useState } from "react";
 import Footer from "./footer/Footer";
 import NavBar from "./nav/NavBar";
 import { Outlet } from "react-router-dom";
-import { MutatingDots } from "react-loader-spinner";
 
 function Layout() {
-  console.log("layout");
-  const [loding, setLoding] = useState(true);
-
-  // setLoding(true)
-
-  console.log(loding);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoding(!loding);
-    }, 2500);
-  }, []);
-
   return (
     <>
-      <div className={`loder-container ${!loding && "d-none"}`}>
-        <MutatingDots visible={true} height="100" width="100" color="#131921" secondaryColor="#131921" radius="12.2" />
-      </div>
-
-        <div className={loding ? "d-none" : "d-block"}>
-          <NavBar />
-          <Outlet />
-          <Footer />
-        </div>
-
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
