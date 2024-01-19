7a69acce2e2371f8193f8775910ee920751cdc52
Yehia <yehia.zez2@gmail.com>
2024-01-19 17:18:12 +0200

    My Check Out Page

---
+++ src/components/Check out/usersData.js
@@ -0,0 +1,44 @@
+
+export let CheckOutData = [
+    {
+        "id": "",
+        "name": "Yehia",
+        "email": "",
+        "address": "mataria city plane",
+        "city": "Cairo",
+        "phone": "0123456789",
+        "Currency": "EGP ",
+        "cart": [
+            {
+                "id": "1",
+                "category_name": "T-shirts",
+                "name_product": "Bantlon",
+                "Description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, repellendus velit reiciendis porro dicta, natus accusantium voluptatibus deleniti veritatis maxime dolorum, tenetur ad tempora aut itaque fuga adipisci perferendis voluptatem!",
+                "img_product": "",
+                "price_product": 500,
+                "number_product": "1",
+            }, {
+                "id": "2",
+                "category_name": "T-shirts",
+                "name_product": "Bantlon",
+                "img_product": "",
+                "price_product": 10,
+                "number_product": "2",
+            }, {
+                "id": "3",
+                "category_name": "T-shirts",
+                "name_product": "Bantlon",
+                "img_product": "",
+                "price_product": 10,
+                "number_product": "2",
+            }, {
+                "id": "3",
+                "category_name": "T-shirts",
+                "name_product": "Bantlon",
+                "img_product": "",
+                "price_product": 10,
+                "number_product": "2",
+            },
+        ]
+    }
+];
