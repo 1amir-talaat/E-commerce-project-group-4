0dddeb9ae4aa8cce5554eeb0b8e957e3ab001baa
Adam Ahmed <130517274+Frisky0A@users.noreply.github.com>
2024-01-21 04:52:32 +0200

    Updated
    Importing Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,24 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home/Home";
 import ProductInfo from "./pages/ProductInfo/ProductInfo";
 import Cart from "./pages/Cart/Cart";
+import { CartProvider } from "./pages/Cart/Cartlogic";
 import Products from "./pages/Products/Products";
 import CheckOut from "./pages/CheckOut/CheckOut";
 import Register from "./pages/Register/Register";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="product-info" element={<ProductInfo />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="products" element={<Products />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-      <Route path="/check-out" element={<CheckOut />} />
-    </Routes>
+    <CartProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="productinfo/:id" element={<ProductInfo />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Products />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/check-out" element={<CheckOut />} />
+      </Routes>
+    </CartProvider>
   );
 }
 
